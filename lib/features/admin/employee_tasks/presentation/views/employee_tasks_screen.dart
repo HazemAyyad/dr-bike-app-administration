0e70a2f28073622253b9d90580f60b99bb7b4110
HazemAyyad <hazem1fadil@gmail.com>
2026-08-03 21:07:52 +0300

    Use icon tabs for employee tasks

--- lib/features/admin/employee_tasks/presentation/views/employee_tasks_screen.dart
+++ lib/features/admin/employee_tasks/presentation/views/employee_tasks_screen.dart
@@ -3,9 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 import '../../../../../core/helpers/custom_app_bar.dart';
-import '../../../../../core/helpers/costom_dialog_filter.dart';
 import '../widgets/employee_tasks_fab_lens.dart';
-import '../../../../../core/helpers/custom_tab_bar.dart';
 import '../../../../../core/services/theme_service.dart';
 import '../../../../../core/utils/app_colors.dart';
 import '../controllers/employee_tasks_controller.dart';
@@ -74,28 +72,6 @@ class EmployeeTasksScreen extends GetView<EmployeeTasksController> {
                 color: Colors.red,
               ),
             ),
-          IconButton(
-            tooltip: 'filter'.tr,
-            onPressed: () {
-              showCustomDialog(
-                context,
-                fromDateController: controller.fromDateController,
-                toDateController: controller.toDateController,
-                employeeNameController: controller.employeeNameController,
-                label: 'employeeTasksSearch',
-                onPressed: () {
-                  controller.filterEmployeeTasks();
-                  Get.back();
-                },
-              );
-            },
-            icon: Icon(
-              Icons.calendar_today_outlined,
-              color: ThemeService.isDark.value
-                  ? AppColors.primaryColor
-                  : AppColors.secondaryColor,
-            ),
-          ),
           SizedBox(width: 8.w),
         ],
       ),
@@ -106,11 +82,7 @@ class EmployeeTasksScreen extends GetView<EmployeeTasksController> {
           physics: kRefreshableScrollPhysics,
           slivers: [
             SliverToBoxAdapter(
-              child: AppTabs(
-                tabs: controller.tabs,
-                currentTab: controller.currentTab,
-                changeTab: controller.changeTab,
-              ),
+              child: _EmployeeTaskIconTabs(controller: controller),
             ),
             GetBuilder<EmployeeTasksController>(
               id: 'searchBar',
@@ -209,6 +181,113 @@ class EmployeeTasksScreen extends GetView<EmployeeTasksController> {
       floatingActionButtonLocation: Get.locale!.languageCode == 'ar'
           ? FloatingActionButtonLocation.startFloat
           : FloatingActionButtonLocation.endFloat,
+    );
+  }
+}
+
+class _EmployeeTaskIconTabs extends StatelessWidget {
+  const _EmployeeTaskIconTabs({required this.controller});
+
+  final EmployeeTasksController controller;
+
+  IconData _iconForIndex(int index) {
+    if (index == controller.archiveTabIndex) {
+      return Icons.archive_outlined;
+    }
+    if (index == controller.completedTabIndex) {
+      return Icons.check_circle_outline_rounded;
+    }
+    if (controller.canReviewTasks && index == 1) {
+      return Icons.rate_review_outlined;
+    }
+    return Icons.playlist_add_check_rounded;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => Container(
+        margin: EdgeInsets.fromLTRB(12.w, 8.h, 12.w, 4.h),
+        padding: EdgeInsets.all(4.w),
+        decoration: BoxDecoration(
+          color: ThemeService.isDark.value
+              ? AppColors.customGreyColor
+              : AppColors.whiteColor2,
+          borderRadius: BorderRadius.circular(12.r),
+        ),
+        child: Row(
+          children: [
+            for (var i = 0; i < controller.tabs.length; i++)
+              Expanded(
+                child: _EmployeeTaskIconTab(
+                  icon: _iconForIndex(i),
+                  label: controller.tabs[i].tr,
+                  selected: controller.currentTab.value == i,
+                  onTap: () => controller.changeTab(i),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _EmployeeTaskIconTab extends StatelessWidget {
+  const _EmployeeTaskIconTab({
+    required this.icon,
+    required this.label,
+    required this.selected,
+    required this.onTap,
+  });
+
+  final IconData icon;
+  final String label;
+  final bool selected;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final fg = selected
+        ? Colors.white
+        : ThemeService.isDark.value
+            ? Colors.white70
+            : AppColors.secondaryColor;
+    return Tooltip(
+      message: label,
+      child: InkWell(
+        borderRadius: BorderRadius.circular(9.r),
+        onTap: onTap,
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 160),
+          height: 44.h,
+          padding: EdgeInsets.symmetric(horizontal: 4.w),
+          decoration: BoxDecoration(
+            color: selected ? AppColors.operationalPurple : Colors.transparent,
+            borderRadius: BorderRadius.circular(9.r),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(icon, size: 18.sp, color: fg),
+              SizedBox(width: 4.w),
+              Flexible(
+                child: Text(
+                  label,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 10.5.sp,
+                    fontWeight: selected ? FontWeight.w700 : FontWeight.w600,
+                    color: fg,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
