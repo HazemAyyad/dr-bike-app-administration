fa32da5989fc69831589d0e9647a11aa2addfafc
HazemAyyad <hazem1fadil@gmail.com>
2026-08-17 23:23:24 +0300

    Fix reports API dependency binding

--- lib/features/admin/reports/presentation/bindings/reports_binding.dart
+++ lib/features/admin/reports/presentation/bindings/reports_binding.dart
@@ -1,14 +1,16 @@
 import 'package:get/get.dart';
 
-import '../../../../../core/databases/api/api_consumer.dart';
+import '../../../../../core/databases/api/dio_consumer.dart';
+import '../../../../../core/services/app_dependency_registry.dart';
 import '../../data/reports_api_service.dart';
 import '../controllers/reports_controller.dart';
 
 class ReportsBinding extends Bindings {
   @override
   void dependencies() {
+    AppDependencyRegistry.ensureNetworkAndApi();
     Get.lazyPut(
-      () => ReportsApiService(api: Get.find<ApiConsumer>()),
+      () => ReportsApiService(api: Get.find<DioConsumer>()),
       fenix: true,
     );
     Get.lazyPut(
